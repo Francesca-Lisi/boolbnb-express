f9a0c81629ae67f261c2e2b8b9b236523a78f536
joelepore <giuseppejoelepore@gmail.com>
2025-02-19 17:39:52 +0100

    feat: implement index route

--- controllers/propertiesController.js
+++ controllers/propertiesController.js
@@ -1,9 +1,22 @@
 const connect = require("../data/db")
 
-
+/*
+    Selezionare id, title, address, likes, avg(reviews), n. tot. recensioni, la prima immagine
+    dalle tabelle properties, reviews, images
+*/
 const index = (req, res) => {
-    const sql = ""
-    res.send("elenco degli immobili")
+    const sql = `
+        SELECT properties.id, properties.title, properties.address, properties.likes, round(avg(reviews.vote), 1) AS average_vote, COUNT(reviews.id) AS number_of_reviews , images.path AS image
+        FROM properties
+        LEFT JOIN reviews ON reviews.property_id = properties.id
+        LEFT JOIN images ON images.property_id = properties.id
+        GROUP BY properties.id, image
+        ORDER BY likes DESC
+    `
+    connect.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err.message });
+        res.json(results);
+    });
 }
 
 const show = (req, res) => {
