30a691c19eeec730d52d74afc7090284a66cd452
Francesca-Lisi <francescalisi2143@gmail.com>
2025-02-20 17:22:05 +0100

    feat: add sanitizeData in middlewares

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require("cors")
 const notFound = require("./middlewares/notFound")
 const errorsHandler = require("./middlewares/errorsHandler")
 const setMainUrl = require('./middlewares/setMainUrl')
+const sanitizeData = require('./middlewares/sanitizeData')
 
 const propertiesRouter = require("./routers/propertiesRouter")
 const typesRouter = require("./routers/typesRouter")
@@ -17,6 +18,8 @@ app.use(setMainUrl)
 app.use(cors({
     origin: process.env.CORS_ORIGIN
 }))
+
+app.use(sanitizeData)
 
 app.get("/", (req, res) => {
     res.send("server connesso")
